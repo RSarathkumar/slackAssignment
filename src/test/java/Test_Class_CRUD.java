cf9bee2d77cd04cdceea968c01840635b8533712
Sarath <sarath@securly.com>
2020-04-20 03:16:43 +0530

    Final changes for naming standards

--- src/test/java/Test_Class_CRUD.java
+++ src/test/java/Test_Class_CRUD.java
@@ -20,6 +20,7 @@ public class Test_Class_CRUD {
 
     @BeforeTest
     public void setUp() {
+        //Initialising all the helper class to support generate data (payload for channel crud activities)
         fakerUtils =  new FakerUtils();
         SlackChannelPOJO = fakerUtils.createChannelPayload();
         faker =  new Faker();
@@ -28,7 +29,9 @@ public class Test_Class_CRUD {
     @Test(priority = 3)
     public void getListOfChannels(){
         response = executor.get("https://slack.com/api/channels.list");
+        //Printing the response body for small approval
         System.out.println("Response body is " + response.getBody().asString());
+        //Getting the specific value of the response using the key with jsonPath
         jsonPath = response.jsonPath();
         Assert.assertEquals(jsonPath.getString("ok"),"true");
     }
@@ -41,6 +44,7 @@ public class Test_Class_CRUD {
 
         System.out.println("Response body is " + response.getBody().asString());
         jsonPath = response.jsonPath();
+        //Recording the id generated for channel and replacing the payload with id for further CRUD operations
         SlackChannelPOJO.setChannel(jsonPath.getString("channel.id"));
         Assert.assertEquals(jsonPath.getString("ok"),"true");
     }
@@ -57,6 +61,7 @@ public class Test_Class_CRUD {
 
     @Test(priority = 2)
     public void renameCreatedChannel(){
+        //Changing the payload(name of the channel) created to rename via API
         SlackChannelPOJO.setName(faker.name().firstName().toLowerCase());
         channelPayload = fakerUtils.prepareChannelPayload(SlackChannelPOJO);
         response = executor.post("https://slack.com/api/conversations.rename",channelPayload);
@@ -67,7 +72,7 @@ public class Test_Class_CRUD {
 
     @Test(priority = 4)
     public void archiveCreatedChannel(){
-
+        //I am archiving the channel which I renamed
         channelPayload = fakerUtils.prepareChannelPayload(SlackChannelPOJO);
         response = executor.post("https://slack.com/api/conversations.archive",channelPayload);
         System.out.println("Response body is " + response.getBody().asString());
@@ -77,7 +82,7 @@ public class Test_Class_CRUD {
 
     @Test(priority = 5)
     public void verifyArchivedChannel(){
-
+        //I am trying to join the archived channel. So it would give me error and it verifies the archiving of channel
         channelPayload = fakerUtils.prepareChannelPayload(SlackChannelPOJO);
         response = executor.post("https://slack.com/api/conversations.join",channelPayload);
         System.out.println("Response body is " + response.getBody().asString());
